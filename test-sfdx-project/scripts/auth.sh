2b1e5dfefb52858722b92b33a2b18e73a7e441d1
Jakob Skogby <jakobskogby95@gmail.com>
2021-10-13 15:47:54 +0200

    Fixed spelling

--- test-sfdx-project/scripts/auth.sh
+++ test-sfdx-project/scripts/auth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [$1 == "prod"]; then
+if [ $1 == "prod" ]; then
 sfdx force:auth:web:login --setalias prod --instanceurl https://login.salesforce.com
-elif [ $1 == "stage"]; then
+elif [ $1 == "stage" ]; then
 sfdx force:auth:web:login --setalias stage --instanceurl https://test.salesforce.com
 else 
 sfdx force:auth:web:login --setalias $1 --instanceurl https://test.salesforce.com --setdefaultusername
